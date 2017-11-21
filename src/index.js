8a2fb7c46acb274f3cc2c7975b210bf8eb87b187
Simon <sima@chetverukhin.ru>
2017-11-21 21:31:27 +0300

    feat(step3): add eslint, codeclimate & travis

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import readlineSync from 'readline-sync';
 
 const game = () => {
-    console.log('Welcome to the Brain Games!\n');
-    let userName = readlineSync.question('May I have your name? ');
-    console.log(`Hello, ${userName}!`);
-}
+  console.log('Welcome to the Brain Games!\n');
+  const userName = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${userName}!`);
+};
 
 export default game;
