7a5afad3172e666780b58b38a1252a4902a3d302
Simon <sima@chetverukhin.ru>
2017-11-25 19:49:43 +0300

    fix(*) pair -> numbers

--- src/games/brain-gcd.js
+++ src/games/brain-gcd.js
@@ -4,18 +4,16 @@ import { getRandomNumber } from '../utils';
 
 const welcomeMessage = 'Find the greatest common divisor of given numbers.';
 
-const gcd = (pair) => {
-  const a = car(pair);
-  const b = cdr(pair);
+const gcd = (a, b) => {
   if (b === 0) return a;
-  return gcd(cons(b, a % b));
+  return gcd(b, a % b);
 };
 
 export const brainGcd = () => {
   const getQuestion = () => {
     const numbersPair = cons(getRandomNumber(10, 100), getRandomNumber(10, 100));
     const question = `${car(numbersPair)} ${cdr(numbersPair)}`;
-    const correctAnswer = gcd(numbersPair);
+    const correctAnswer = gcd(car(numbersPair), cdr(numbersPair));
     return cons(question, correctAnswer);
   };
 
